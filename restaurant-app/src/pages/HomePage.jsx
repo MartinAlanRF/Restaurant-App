367f35e31ffb506b0e02eb5e0cfd248445c7a4c9
Martin Alan Rodriguez Fisher <marodriguez.dwfc6@ucamp.utel.edu.mx>
2022-07-25 12:39:24 -0500

    Posicionamientos de etiquetas HomePage

--- restaurant-app/src/pages/HomePage.jsx
+++ restaurant-app/src/pages/HomePage.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import imageVeracruz from "../assets/veracruz.jpg";
 
+import imageCaurosel1 from "../assets/comida/Arroz-a-la-tumbada.jpg";
+import imageCaurosel2 from "../assets/comida/Chilpachole-de-jaiba.jpg";
+import imageCaurosel3 from "../assets/comida/pescadoVeracruzana.jpg";
+import imageCaurosel4 from "../assets/comida/zacahuil1.jpg";
+
+
+
 const HomePage = () => {
   return (
     <>
@@ -12,10 +19,37 @@ const HomePage = () => {
             </h1>
           </div>
           <div className="col-12 mt-2  mx-auto">
-            <div className="row mx-auto">
+
+            <div className="row">
+              <div className="col-lg-4 col-md-4 col-sm-12 ">
+                <div id="estado" className="container ">
+                  <img
+                    id="veracruz"
+                    className=""
+                    src={imageVeracruz}
+                    alt="Imagen del estado de Veracruz, México"
+                  />
+                </div>
+              </div>
+              <div className="col-lg-8 col-md-8 col-sm-12">
+                <div className="container">
+                  <div id="jumbotron" className="p-3 text-white rounded ">
+                    <h1 className="display-6 text-center">
+                      PEDACITO DE VERACRUZ
+                    </h1>
+                    <p className="">
+                      En este restaurante te esperan algunos de los platillos
+                      mas delicioso de la región de Veracruz, desde la zona norte del estado,
+                      como lo es panuco bajando hasta la zona sur del mismo Minatitlan.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
               <div
                 id="carousel"
-                className="carousel slide mb-2"
+                className="carousel slide w-75 mx-auto"
                 data-bs-interval={3000}
                 data-bs-ride="carousel"
               >
@@ -50,37 +84,45 @@ const HomePage = () => {
                     </div>
                   </div>
 
-                  <div className="carousel-item active ">
+                  <div className="carousel-item active  ">
                     <img
-                      src="https://via.placeholder.com/900x200?text=PRIMERA"
+                      src={imageCaurosel1}
                       alt=""
-                      className="d-block w-100"
+                      className="d-block w-100 img-flui"
                     />
                     <div className="carousel-caption d-none d-md-block">
-                      <h5>Primera imagen</h5>
-                      <p>Texto para mostrar como practiva del slider</p>
+                      <h5>ARROZ A LA TUMBADA</h5>
                     </div>
                   </div>
                   <div className="carousel-item">
                     <img
-                      src="https://via.placeholder.com/900x200?text=SEGUNDA"
+                      src={imageCaurosel2}
                       alt=""
-                      className="d-block w-100"
+                      className="d-block w-100 img-fluid "
                     />
+                    <div className="carousel-caption d-none d-md-block">
+                      <h5>CHILPACHOLE DE JAIBA</h5>
+                    </div>
                   </div>
                   <div className="carousel-item">
                     <img
-                      src="https://via.placeholder.com/900x200?text=TERCERA"
+                      src={imageCaurosel3}
                       alt=""
-                      className="d-block w-100"
+                      className="d-block w-100 img-fluid"
                     />
+                    <div className="carousel-caption d-none d-md-block">
+                      <h5>PESCADO A LA VERACRUZANA</h5>
+                    </div>
                   </div>
                   <div className="carousel-item">
                     <img
-                      src="https://via.placeholder.com/900x200?text=CUARTA"
+                      src={imageCaurosel4}
                       alt=""
-                      className="d-block w-100"
+                      className="d-block w-100 img-fluid"
                     />
+                    <div className="carousel-caption d-none d-md-block">
+                      <h5>ZACAHUIL</h5>
+                    </div>
                   </div>
                 </div>
 
@@ -104,49 +146,10 @@ const HomePage = () => {
                 </button>
               </div>
             </div>
-            <div className="row">
-              <div className="col-lg-8 col-md-8 col-sm-12">
-                <div className="container">
-                  <div id="jumbotron" className="p-3 text-white rounded ">
-                    <h1 className="display-6">
-                      BIENVENIDOS PEDACITO DE VERACRUZ
-                    </h1>
-                    <p>
-                      En este restaurante te esperan algunos de los platillos
-                      mas delicioso de la región de Veracruz, desde la zona norte del estado, 
-                      como lo es panuco bajando hasta la zona sur del mismo Minatitlan.
-                    </p>
-                    <hr className="my-2" />
-                    <div id="botonsJumbotron" className="row">
-                      <div className="col-6">
-                        <a href="/" className="btn btn-success">
-                          Menú
-                        </a>
-                      </div>
-                      <div className="col-6">
-                        <a href="/" className="btn btn-success">
-                          Reservación
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-4 col-sm-12">
-                <div id="estado" className="container">
-                  <img
-                    id="veracruz"
-                    className="img-fluid"
-                    src={imageVeracruz}
-                    alt="Imagen del estado de Veracruz, México"
-                  />
-                </div>
-              </div>
-            </div>
             <div className="row mx-auto mt-4">
-              <div className="col-lg-4 h-25">
+              <div className="col-lg-4">
                 <div id="location">
-                  <h5>¿Donde nos ubicamos?</h5>
+                  <h2>¿Donde nos ubicamos?</h2>
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9574.923064887667!2d-96.93203505723477!3d19.553011423316423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2e2298badf87%3A0x2fee5115e2d809ae!2sBenem%C3%A9rita%20Escuela%20Normal%20Veracruzana%20Enrique%20C.%20Rebsamen!5e0!3m2!1ses-419!2smx!4v1657842123653!5m2!1ses-419!2smx"
                     width={600}
@@ -160,18 +163,24 @@ const HomePage = () => {
                 </div>
               </div>
               <div className="col-lg-8">
-                <section>
-                  <h1>El PEDACITO DE VERACRUZ</h1>
-                  <h3>Fundado en 2022</h3>
-                  <p>
-                    Somos un restaurante creado con fines de compartir la gastronomía
-                    del estado de veracruz. 
-                  </p>
-                  <p>
-                    El objetivo nuestros platillos es que con ellos
-                    nuestos comensales puedan realizar un viaje por la región veracruzana através 
-                    de su palados.
-                  </p>
+                <section id="acercade">
+                  <h2>ACERCA DE NOSOTROS</h2>
+                  <div className="card">
+                    <div className="card-body">
+                      <h4 className="card-title">
+                        El PEDACITO DE VERACRUZ
+                      </h4>
+                      <h6>Fundado en 2022</h6>
+                      <p className="card-text">                    
+                      Somos un restaurante creado con fines de compartir la gastronomía
+                      del estado de veracruz.</p>
+                      <p>
+                        El objetivo nuestros platillos es que con ellos
+                        nuestos comensales puedan realizar un viaje por la región veracruzana através
+                        de su palados.
+                      </p>
+                    </div>
+                  </div>
                 </section>
               </div>
             </div>
